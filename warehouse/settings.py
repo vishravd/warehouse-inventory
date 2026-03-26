0d4a62150e7b02fddf09e6c11704df86807fdeaf
Vishwa Dharmasena <vishravd@gmail.com>
2026-03-26 23:17:23 +0530

    Fix static files storage and collectstatic for Railway

--- warehouse/settings.py
+++ warehouse/settings.py
@@ -130,7 +130,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
+    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"},
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
